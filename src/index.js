45900c95282959eb3f09d72b62c9aa4babe194ae
Anshul Sachdev <anshul16.sachdev@gmail.com>
2023-06-13 09:54:36 +0530

    Version4 : Basic Counter App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const App = () => {
-    return <div>This text is from reactjs component</div>
+const Counter = () => {
+    const [count, setCount] = React.useState(0)    
+
+    const handleClick = (e) => {
+        setCount(count => count+1)
+    }
+
+    return (
+        <div>
+            <div>Counter : {count}</div>
+            <button onClick={handleClick}>Increse by 1</button>
+        </div>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
-root.render(<App />);
+root.render(<Counter />);
